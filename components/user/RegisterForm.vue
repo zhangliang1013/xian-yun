c5d409ecd671d4d058fd572cf34f5beb8b1ab1d1
张亮 <2286377528@qq.com>
2020-02-09 13:45:53 +0800

    12,注册页面的数据双向绑定完成，并实现表单验证功能

--- components/user/RegisterForm.vue
+++ components/user/RegisterForm.vue
@@ -1,27 +1,28 @@
 <template>
   <el-form :model="form" ref="form" :rules="rules" class="form">
-    <el-form-item class="form-item">
-      <el-input placeholder="用户名手机"></el-input>
+    <!-- 用户账户 -->
+    <el-form-item class="form-item" prop="username">
+      <el-input placeholder="用户名手机" v-model="form.username"></el-input>
     </el-form-item>
-
-    <el-form-item class="form-item">
-      <el-input placeholder="验证码">
+    <!-- 验证码 -->
+    <el-form-item class="form-item" prop="captcha">
+      <el-input placeholder="验证码" v-model="form.captcha">
         <template slot="append">
           <el-button @click="handleSendCaptcha">发送验证码</el-button>
         </template>
       </el-input>
     </el-form-item>
-
-    <el-form-item class="form-item">
-      <el-input placeholder="用户名"></el-input>
+    <!-- 用户名 -->
+    <el-form-item class="form-item" prop="nickname">
+      <el-input placeholder="用户名" v-model="form.nickname"></el-input>
     </el-form-item>
-
-    <el-form-item class="form-item">
-      <el-input placeholder="密码" type="password"></el-input>
+    <!-- 密码 -->
+    <el-form-item class="form-item" prop="password">
+      <el-input placeholder="密码" type="password" v-model="form.password"></el-input>
     </el-form-item>
-
-    <el-form-item class="form-item">
-      <el-input placeholder="确认密码" type="password"></el-input>
+    <!--   -->
+    <el-form-item class="form-item" prop="checkPassword">
+      <el-input placeholder="确认密码" type="password" v-model="form.checkPassword "></el-input>
     </el-form-item>
 
     <el-button class="submit" type="primary" @click="handleRegSubmit">注册</el-button>
@@ -30,18 +31,64 @@
 
 <script>
 export default {
+  // 输入密码的自定义验证
   data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.form.checkPassword !== "") {
+          this.$refs.form.validateField("checkPassword");
+        }
+        callback();
+      }
+    };
+    // 输入确认密码的自定义验证
+    var validateCheckPass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.form.password) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+    // 输入验证账户名的自定义验证
+    var validateUsername = (rule,value,callback) =>{
+        if(value == ''){
+             callback(new Error("请输入账户名"));
+        }else if(/^[1]\d{10}$/.test(value)){
+              callback()
+        }else {
+            callback(new Error("输入的账户名不合法"));
+        }
+    }
     return {
-      form: {}
+      form: {
+        username: "",
+        captcha: "",
+        nickname: "",
+        password: "",
+        checkPassword: ""
+      },
+      rules: {
+        username: [
+          { validator: validateUsername, trigger: "blur" }
+        ],
+        captcha: [{ required: true, message: "请输入验证码", trigger: "blur" }],
+        nickname: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        password: [{ validator: validatePass, trigger: "blur" }],
+        checkPassword: [{ validator: validateCheckPass, trigger: 'blur' }]
+      }
     };
   },
   methods: {
     //点击验证码
     handleSendCaptcha() {},
     // 点击注册按钮
-    handleRegSubmit(){
-
-    }
+    handleRegSubmit() {}
   }
 };
 </script>
