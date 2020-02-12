c480ea9d2c78eac23a0296c8a201a49292e62ece
张亮 <2286377528@qq.com>
2020-02-12 16:03:15 +0800

    31,机票列表页引入头部组件

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,15 +1,60 @@
 <template>
-  <div class="search">
-      这是机票搜索成功的展示页面（机票展示）
-  </div>
+    <section class="contianer">
+        <el-row  type="flex" justify="space-between">
+
+            <!-- 顶部过滤列表 -->
+            <div class="flights-content">
+                <!-- 过滤条件 -->
+                <div>
+                    
+                </div>
+                
+                <!-- 航班头部布局 -->
+                <FlightsListHead></FlightsListHead>
+                
+                
+                <!-- 航班信息 -->
+                <div>
+                    
+                </div>
+            </div>
+
+            <!-- 侧边栏 -->
+            <div class="aside">
+                <!-- 侧边栏组件 -->
+            </div>
+        </el-row>
+    </section>
 </template>
 
 <script>
-export default {
+import FlightsListHead from '@/components/air/flightsListHead';
 
+
+export default {
+  components :{
+    FlightsListHead
+  },
+    data(){
+        return {
+            
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped lang="less">
+    .contianer{
+        width:1000px;
+        margin:20px auto;
+    }
 
+    .flights-content{
+        width:745px;
+        font-size:14px;
+    }
+
+    .aside{
+        width:240px;
+    } 
 </style>
